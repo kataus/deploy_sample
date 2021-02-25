ad1d3fb2e31d3cdc6e3f371434f18351c28bfc48
kataus <kataus@gmail.com>
2021-02-25 10:10:30 +0300

    add new page public.html

--- src/main/java/ru/otus/controller/IndexController.java
+++ src/main/java/ru/otus/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/public")
+    public String publicPage(){
+        return "public";
+    }
 }
